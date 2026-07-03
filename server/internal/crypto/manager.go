b7ce174a0e2fbd28ab0945f9fa0b0a42da77d168
post-kserks <upostnov@mail.ru>
2026-07-03 22:00:08 +0300

    feat: add EncryptionManager core for TDE

---
+++ server/internal/crypto/manager.go
@@ -0,0 +1,53 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+type EncryptionManager struct {
+	dek   []byte      // 32 bytes for AES-256
+	aead  cipher.AEAD
+	keyID string
+}
+
+func NewEncryptionManager(dek []byte, keyID string) (*EncryptionManager, error) {
+	if len(dek) != 32 {
+		return nil, fmt.Errorf("DEK must be 32 bytes, got %d", len(dek))
+	}
+	block, err := aes.NewCipher(dek)
+	if err != nil {
+		return nil, err
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	return &EncryptionManager{dek: dek, aead: aead, keyID: keyID}, nil
+}
+
+func (em *EncryptionManager) EncryptPage(plaintext []byte, pageID []byte) (nonce, ciphertext []byte, err error) {
+	nonce = make([]byte, em.aead.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, nil, err
+	}
+	ciphertext = em.aead.Seal(nil, nonce, plaintext, pageID)
+	return nonce, ciphertext, nil
+}
+
+func (em *EncryptionManager) DecryptPage(nonce, ciphertext []byte, pageID []byte) ([]byte, error) {
+	return em.aead.Open(nil, nonce, ciphertext, pageID)
+}
+
+func (em *EncryptionManager) KeyVersion() uint32 {
+	return 1 // simplified
+}
+
+func (em *EncryptionManager) Zeroize() {
+	for i := range em.dek {
+		em.dek[i] = 0
+	}
+}
---
+++ server/internal/crypto/manager_test.go
@@ -0,0 +1,120 @@
+package crypto
+
+import (
+	"bytes"
+	"crypto/rand"
+	"testing"
+)
+
+func TestEncryptDecryptRoundtrip(t *testing.T) {
+	dek := make([]byte, 32)
+	if _, err := rand.Read(dek); err != nil {
+		t.Fatal(err)
+	}
+
+	em, err := NewEncryptionManager(dek, "test-key-1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	plaintext := []byte("Hello, World! This is a test page.")
+	pageID := []byte("page-123")
+
+	nonce, ciphertext, err := em.EncryptPage(plaintext, pageID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decrypted, err := em.DecryptPage(nonce, ciphertext, pageID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(plaintext, decrypted) {
+		t.Errorf("decrypted text does not match original")
+	}
+}
+
+func TestDecryptWithWrongKeyFails(t *testing.T) {
+	dek1 := make([]byte, 32)
+	dek2 := make([]byte, 32)
+	if _, err := rand.Read(dek1); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := rand.Read(dek2); err != nil {
+		t.Fatal(err)
+	}
+
+	em1, err := NewEncryptionManager(dek1, "key-1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	em2, err := NewEncryptionManager(dek2, "key-2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	plaintext := []byte("Secret data")
+	pageID := []byte("page-456")
+
+	nonce, ciphertext, err := em1.EncryptPage(plaintext, pageID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = em2.DecryptPage(nonce, ciphertext, pageID)
+	if err == nil {
+		t.Error("expected decryption with wrong key to fail")
+	}
+}
+
+func TestDecryptWithTamperedDataFails(t *testing.T) {
+	dek := make([]byte, 32)
+	if _, err := rand.Read(dek); err != nil {
+		t.Fatal(err)
+	}
+
+	em, err := NewEncryptionManager(dek, "test-key")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	plaintext := []byte("Important data")
+	pageID := []byte("page-789")
+
+	nonce, ciphertext, err := em.EncryptPage(plaintext, pageID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Tamper with ciphertext
+	tampered := make([]byte, len(ciphertext))
+	copy(tampered, ciphertext)
+	tampered[0] ^= 0xFF
+
+	_, err = em.DecryptPage(nonce, tampered, pageID)
+	if err == nil {
+		t.Error("expected decryption with tampered data to fail")
+	}
+}
+
+func TestZeroize(t *testing.T) {
+	dek := make([]byte, 32)
+	for i := range dek {
+		dek[i] = byte(i)
+	}
+
+	em, err := NewEncryptionManager(dek, "test-key")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	em.Zeroize()
+
+	for i, b := range em.dek {
+		if b != 0 {
+			t.Errorf("dek[%d] = %d, expected 0", i, b)
+		}
+	}
+}
